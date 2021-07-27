bf04e599370c19bbdaadbbcc0876b4f113eca48d
Marc Wenning-Künne <marcwenningkuenne@gmail.com>
2021-07-27 20:13:30 +0200

    Adding the Calculator Class and the Constructor

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+    }
+}
