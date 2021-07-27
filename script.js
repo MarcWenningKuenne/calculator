1ed41d0f50e104814e42e33f133e895cb1edc9dc
Marc Wenning-Künne <marcwenningkuenne@gmail.com>
2021-07-27 20:23:43 +0200

    Add EventListener to the buttons

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
-const numberButtons = document.querySelectorAll('[data-number]')
-const operationButtons = document.querySelectorAll('[data-operation]')
-const equalsButton = document.querySelector('[data-equals]')
-const deleteButton = document.querySelector('[data-delete]')
-const allClearButton = document.querySelector('[data-all-clear]')
-const previousOperandTextElement = document.querySelector('[data-previous-operand]')
-const currentOperandTextElement = document.querySelector('[data-current-operand]')
-
 class Calculator {
     constructor(previousOperandTextElement, currentOperandTextElement) {
         this.previousOperandTextElement = previousOperandTextElement
         this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
     }
 
     clear(){
-
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
     }
 
     delete(){
@@ -21,7 +16,7 @@ class Calculator {
     }
 
     appendNumber(number){
-
+        this.currentOperand = number
     }
 
     chooseOperation(operation){
@@ -33,6 +28,23 @@ class Calculator {
     }
 
     updateDisplay(){
-        
-    }
+        this.currentOperandTextElement.innerText = this.currentOperand
+    } 
 }
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
